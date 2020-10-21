ce40d29ca8783e34ccd96cff84a0be342176a0da
Roshan Kumar <53408220+roshan9419@users.noreply.github.com>
2020-10-21 08:55:00 +0530

    Update __init__.py

--- gmapFinder/__init__.py
+++ gmapFinder/__init__.py
@@ -1,3 +1,20 @@
-"""This piece of software was last updated on 20-October-2020 at 23:30 IST"""
+"""This piece of software was last updated on 21-October-2020 at 09:00 IST"""
+"""
+Function getCurrentLocation()
+	-Gives the current location of your device, with some details
+	IP, City, Region, Country, Location, Org, Postal, Timezone
 
-from gmapFinder.directions import getDirection, getDistance, getCurrentLocation
+Function getDirection(startingAddress, destinationAddress)
+	-returns a dictionary having:
+		url of Google Map link,
+		total distance in (km, miles),
+		starting point and destination point address and location.
+
+Example::
+	import gmapFinder as gp
+
+	gp.getCurrentLocation()
+	gp.getDirection("Lucknow, UttarPradesh", "Tokyo, Japan")
+"""
+
+from gmapFinder.directions import getDirection, getCurrentLocation
